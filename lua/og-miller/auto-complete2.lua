ad5e7e9c66adcb9e723615a46b436239fda4e9e6
Gavin M <gavin.miller@sinnerschrader.com>
2022-12-06 15:46:11 +0100

    feat(auto-complete): remove autocomplete from vim command line

--- lua/og-miller/auto-complete2.lua
+++ lua/og-miller/auto-complete2.lua
@@ -61,14 +61,14 @@
 --  })
 
 -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-  cmp.setup.cmdline(':', {
-    mapping = cmp.mapping.preset.cmdline(),
-    sources = cmp.config.sources({
-      { name = 'path' }
-    }, {
-      { name = 'cmdline' }
-    })
-  })
+--  cmp.setup.cmdline(':', {
+--    mapping = cmp.mapping.preset.cmdline(),
+--    sources = cmp.config.sources({
+--      { name = 'path' }
+--    }, {
+--      { name = 'cmdline' }
+--    })
+--  })
 
   -- Setup lspconfig.
   local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
